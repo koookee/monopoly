f358d847c69eeca4765bb0cc98b97b3465006adb
andre-Hazim <andre.hazim1@gmail.com>
2021-10-18 21:45:57 -0400

    Created the game board with a card class
    I also added the start of the player class

---
+++ src/Card.java
@@ -0,0 +1,55 @@
+public class Card {
+    private String name;
+    private int cost;
+    private String color;
+
+
+    public Card(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+
+    }
+
+    /**
+     * @author Andre
+     * @param name
+     * @param cost
+     */
+    public Card(String name, int cost,String color) {
+        this(name,cost);
+        this.color = color;
+    }
+
+
+    /**
+     * @author Andre
+     * @param player
+     */
+    public void collectRent(Player player){
+
+    }
+
+    /**
+     * @author Andre
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @author Andre
+     * @return
+     */
+    public int getCost() {
+        return cost;
+    }
+
+    /**
+     * @author Andre
+     * @return
+     */
+    public int getRent() {
+        return (int) (cost * 0.1) ;
+    }
+}
--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Game
 {
-    Parser parser;
-    boolean gameIsOver;
-    boolean inMenu;
+    private Parser parser;
+    private boolean gameIsOver;
+    private boolean inMenu;
+    private Map<Integer, Card> gameBoard;
+
 
     /**
      * @author Hussein
@@ -15,6 +19,28 @@ public class Game
         parser = new Parser();
         gameIsOver = false;
         inMenu = true;
+        gameBoard = new HashMap();
+
+    }
+
+    /**
+     * @author Andre
+     * Creates the game board
+     */
+    public void createGameBoard(){
+
+        String[] streetNames = {"Sparks Street","Lebreton Flats","wellington Street","lauier Avenue","waller Street","bronson Avenue","hurdman Road",
+                "Lett Street","lampman Crescent","macKay Street","slater Street","thompson Street","sweetLand Avenue","sloper Place","perly Drive",
+                "morrison Street","keefer Street","mcLeod Street","rochester Street","parliament Hill","rideau Canal"};
+        int[] costs = {60,60,100,100,120,180,180,200,220,220,240,260,260,280,300,300,320,350,400};
+
+        String[] colors = {"Brown","Brown","light blue","light blue","light blue","pink","pink","pink","orange","orange","orange","yellow","yellow",
+                "yellow","green","green","green","blue","blue"};
+
+        for (int i = 0; i < streetNames.length; i++) {
+            gameBoard.put(i,new Card(streetNames[i],costs[i],colors[i]));
+        }
+
     }
 
     /**
@@ -24,6 +50,7 @@ public class Game
     public void play()
     {
         while (!gameIsOver) {
+            this.createGameBoard();
             Command command = parser.getCommand();
             processCommand(command, 1);
         }
