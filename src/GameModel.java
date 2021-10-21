2ca23df3b239256f77a2a275846bf8c21f5b4617
JackMacKinnon51 <jackmackinnon51@gmail.com>
2021-10-21 14:29:55 -0400

    Start of Game Model

---
+++ src/GameModel.java
@@ -0,0 +1,14 @@
+public class GameModel {
+    private GameModel.Status status;
+    private GameModel.Turn turn;
+    
+
+
+
+    public static enum Status {
+
+    }
+    public static enum Turn{
+
+    }
+}
