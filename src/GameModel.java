0f80c7d654afae89dd260d567d126a2120c29a5e
JackMacKinnon51 <jackmackinnon51@gmail.com>
2021-10-21 15:16:29 -0400

    Updates to GameModel

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,14 +1,66 @@
+import java.util.ArrayList;
+
 public class GameModel {
     private GameModel.Status status;
     private GameModel.Turn turn;
-    
+    private ArrayList<GameModelView> views;
+
+
+    public GameModel(){
+        this.status = GameModel.Status.UNDECIDED;
+        this.turn = GameModel.Turn.P1_TURN;
+        this.views = new ArrayList();
+
+
+
+    }
+    public void addGameModelView(GameModelView view){
+        this.views.add(view);
+    }
+
+    public void removeGameModelView(GameModelView view){
+        this.views.remove(view);
+    }
+
+    public GameModel.Status getStatus(){
+        return this.status;
+    }
+
+    private void changeTurn(){
+        switch (turn) {
+            case P1_TURN:
+                turn = GameModel.Turn.P2_TURN;
+                break;
+            case P2_TURN:
+                turn = GameModel.Turn.P3_TURN;
+                break;
+            case P3_TURN:
+                turn = GameModel.Turn.P4_TURN;
+                break;
+            case P4_TURN:
+                turn = GameModel.Turn.P1_TURN;
+                break;
+        }
+    }
+
+    public void play(){
+
+    }
+
 
 
 
     public static enum Status {
-
+        P1_WINS,
+        P2_WINS,
+        P3_WINS,
+        P4_WINS,
+        UNDECIDED;
     }
     public static enum Turn{
-
+        P1_TURN,
+        P2_TURN,
+        P3_TURN,
+        P4_TURN;
     }
 }
