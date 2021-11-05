e4d0b4ddafc26f854f9527b59a033a47bcc239b8
andre-Hazim <andre.hazim1@gmail.com>
2021-11-05 18:18:14 -0400

    added some icons need to get that working properly
    I also added some more buttons that need some implementation from the controller

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,23 +1,21 @@
+import javax.sql.rowset.BaseRowSet;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
-import java.awt.geom.AffineTransform;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.jar.JarEntry;
+
 
 public class GameFrame extends JFrame implements GameView {
 
-    private Parser parser;
-    private boolean inGame;
-    private boolean inMenu;
+
     private GameModel model;
     private Map<Integer,Card> board;
     private ArrayList<JPanel> squares;
 
-    private GameModel gameModel;
+
     private final int botSquares = 6, leftSquares = 12, topSquares =17, rightSquares = 23;
     private  JPanel mainPanel;
     private JPanel playerPanel;
@@ -29,21 +27,18 @@ public class GameFrame extends JFrame implements GameView {
         this.squares = new ArrayList<>();
         this.board = model.getGameBoard();
 
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1600,1024);
         this.createSquares();
         this.mainPanel = paintBoard();
         this.playerPanel = paintPlayerInfo(model.getActivePlayer());
 
-        // New stuff -----
 
-        parser = new Parser();
-        inMenu = true;
-        inGame = false;
         model = new GameModel();
         model.addGameModelView(this);
 
-        // ---------------
+
 
     }
 
@@ -54,14 +49,29 @@ public class GameFrame extends JFrame implements GameView {
             Card c = this.model.getGameBoard().get(i);
             JPanel square = new JPanel(new BorderLayout());
             square.setBorder(new LineBorder(Color.black));
-            square.setPreferredSize(new Dimension(150,200));
+
             JPanel squareTop = new JPanel();
             squareTop.setBackground(c.getColor());
             JLabel name = new JLabel(c.getName());
-
+            JPanel squareBot = new JPanel();
+            squareBot.setBorder(new LineBorder(Color.black));
 
             squareTop.add(name);
+            if (i>botSquares-1 && i<=leftSquares-1 ||i>topSquares-1 && i<=rightSquares-1  ){
+                square.setPreferredSize(new Dimension(290,150));
+                squareBot.setPreferredSize(new Dimension(290,25));
+            }else{
+                square.setPreferredSize(new Dimension(100,150));
+                squareBot.setPreferredSize(new Dimension(100,25));
+            }
+            JLabel price = new JLabel("$" + c.getCost());
+            if(c.getCost() !=0){
+                squareBot.add(price);
+            }
+
             square.add(squareTop, BorderLayout.PAGE_START);
+            square.add(squareBot, BorderLayout.PAGE_END);
+
 
 
             squares.add(square);
@@ -80,6 +90,7 @@ public class GameFrame extends JFrame implements GameView {
         mainPanel.add(left ,BorderLayout.LINE_START);
         mainPanel.add(top, BorderLayout.PAGE_START);
         mainPanel.add(right, BorderLayout.LINE_END);
+
         for (int i = 0; i < squares.size(); i++) {
             if(i<=botSquares-1){
                 bot.add(squares.get(5-i));
@@ -93,6 +104,7 @@ public class GameFrame extends JFrame implements GameView {
                 right.add(squares.get(i));
             }
         }
+
         return mainPanel;
     }
 
@@ -121,6 +133,12 @@ public class GameFrame extends JFrame implements GameView {
         JButton rollButton = new JButton("Roll");
         rollButton.addActionListener(e -> model.play());
         footerPanel.add(rollButton);
+        JButton buy = new JButton("Buy");
+        JButton pass = new JButton("Pass");
+        JButton nextTurn = new JButton("Next Turn");
+        footerPanel.add(buy);
+        footerPanel.add(pass);
+        footerPanel.add(nextTurn);
         mainPanel.add(footerPanel, BorderLayout.PAGE_END);
         return mainPanel;
 
@@ -133,7 +151,7 @@ public class GameFrame extends JFrame implements GameView {
         this.add(mainPanel,BorderLayout.CENTER);
 
 
-
+        this.revalidate();
         this.setVisible(true);
     }
 
@@ -143,38 +161,17 @@ public class GameFrame extends JFrame implements GameView {
      */
     @Override
     public void handleGameStatusUpdate(GameEvent e) {
-        /*
-        this.gameModel = (GameModel) e.getSource();
-        if (!e.getStatus().equals(GameModel.Status.UNDECIDED)){
-            System.out.println("game is over");
-            inGame = false;
-        }
-        else {
-            System.out.println(gameModel.getActivePlayer().getName() + " rolled " + e.getRoll()[0] + " " + e.getRoll()[1]);
-            System.out.println("The card you are on is " + e.getCard().getName() + " cost: " + e.getCard().getCost() + " color: " + e.getCard().getColor());
 
-            if (!e.getCard().isOwned()) {
-                System.out.println("Would you like to buy this property? or pass");
-                Command command = parser.getCommand();
-                gameModel.processCommand(command, 2);
-                while (!command.getCommandWord().equals("buy") && !command.getCommandWord().equals("pass") && !command.getCommandWord().equals("quit")){
-                    command = parser.getCommand();
-                    gameModel.processCommand(command, 2);
-                }
-            } else {
-                System.out.println(e.getCard().getOwner().getName() + " owns this property lol");
-                gameModel.getActivePlayer().payRent(e.getCard().getOwner(), e.getCard());
-                System.out.println("you paid " + e.getCard().getOwner().getName() + " " + e.getCard().getRent() + " dollars");
-                System.out.println("You now have " + gameModel.getActivePlayer().getMoney() + " dollars");
-            }
-        }
-        
-         */
 
         this.model = (GameModel) e.getSource();
-        System.out.println("doing stuff");
+
         playerPanel = paintPlayerInfo(model.getActivePlayer());
         displayGUI();
+        updatePlayerIcon(model.getActivePlayer());
+    }
+    private void updatePlayerIcon(Player activePlayer){
+        int position = activePlayer.getPosition();
+        
     }
 
 
