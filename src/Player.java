167af98212610be22aee898e12242824b10ae49c
andre-Hazim <andre.hazim1@gmail.com>
2021-11-08 20:30:36 -0500

    fixed icon not removed and icon not showing up sometimes

--- src/Player.java
+++ src/Player.java
@@ -21,7 +21,7 @@ public class Player {
      */
     public Player(String name){
         this.name = name;
-        this.money = 1500;
+        this.money = 200;
         this.position = 0;
         this.playing = true;
         properties = new ArrayList<>();
